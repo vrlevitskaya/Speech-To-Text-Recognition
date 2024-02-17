b3c5277a0a03b5136dfcbf10434fc7bd239eaf4a
levitskya03 <75023086+levitskya03@users.noreply.github.com>
2024-02-17 13:52:19 +0300

    Bot is able to transcribe audio files to text, using Whisper-1 by OpenAI. Also, it is able to translate question in 99 languages.  The ability to give responses will be added later.

--- utils.py
+++ utils.py
@@ -1,13 +1,23 @@
 import whisper
+from deep_translator import GoogleTranslator
 
 
 def transcribe_audio(languages, file_path_micro):
     model = whisper.load_model("base")
     result = model.transcribe(file_path_micro)
-    answered_question = answer_the_question(languages, result["text"])
-    return result["text"], answered_question
+    selected_language = languages.lower()
+    response = answer_the_question(languages, result["text"])
+    translated_question = GoogleTranslator(source='auto', target=selected_language).translate(result["text"])
+    return translated_question, response
 
 
-def answer_the_question(languages,question):
-    return f'Привет: {question}'
+def answer_the_question(languages, question):
+    selected_language = languages.lower()
+    answer_to_return = answer(question)
+    translated_answer = GoogleTranslator(source='auto', target=selected_language).translate(answer_to_return)
+    return translated_answer
 
+
+def answer(question):
+    answer_for_question = 'Пока я не умею отвечать на вопросы'
+    return answer_for_question
