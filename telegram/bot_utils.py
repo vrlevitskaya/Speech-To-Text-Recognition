d610f9c863b738bd5474ff0de127277dee85cf91
levitskya03 <75023086+levitskya03@users.noreply.github.com>
2024-03-06 18:34:38 +0300

    Bot has another appearance - as TelegramBot with feedback grid

--- telegram/bot_utils.py
+++ telegram/bot_utils.py
@@ -1,21 +1,21 @@
 import whisper
 from deep_translator import GoogleTranslator
+from parser import get_text_from_google
+from telegram.gpt import generate_answer
 
 
-def transcribe_audio(file_path_micro, selected_language):
+def transcribe_audio(file_path_micro, selected_language, campus='Moscow'):
     model = whisper.load_model("base")
     result = model.transcribe(file_path_micro)
-    translated_question = GoogleTranslator(source='auto', target=selected_language).translate(result["text"])
-    response = answer_the_question(translated_question, selected_language)
+    translated_question = GoogleTranslator(source='auto', target='ru').translate(result["text"])
+    response = answer_the_question(translated_question, selected_language, campus)
     return response
 
 
-def answer_the_question(question, selected_language):
-    answer_to_return = answer(question, selected_language)
-    translated_answer = GoogleTranslator(source='auto', target=selected_language).translate(answer_to_return)
+def answer_the_question(question, selected_language, campus='Moscow'):
+    full_question = question + "HSE University" + " campus"
+    content = get_text_from_google(query=full_question)
+    answer_to_return = generate_answer(question, campus, content)
+    translated_answer = GoogleTranslator(source='auto', target=selected_language).translate(answer_to_return[0])
     return translated_answer
 
-
-def answer(question, selected_language):
-    answer_for_question = 'Пока я не умею отвечать на вопросы'
-    return answer_for_question
---
+++ telegram/gpt.py
@@ -0,0 +1,48 @@
+import requests
+from docx import Document
+from deep_translator import GoogleTranslator
+
+url = "http://localhost:1337/v1/chat/completions"
+
+
+def read_docx(file_path):
+    document = Document(file_path)
+    content = []
+    for paragraph in document.paragraphs:
+        content.append(paragraph.text)
+    return content
+
+
+def translate_content(content):
+    translated_content = []
+    translator = GoogleTranslator(source='ru', target='en')
+    for fragment in content:
+        translated_fragment = translator.translate(text=fragment)
+        translated_content.append(translated_fragment)
+    return translated_content
+
+
+def generate_answer(question, campus, content):
+    answer = []
+    for i in range(len(content)):
+        body = {
+            "model": "gpt-4-turbo",
+            "stream": False,
+            'messages': [{'role': 'system', 'content': f"You are an intelligent assistant helping foreign applicants "
+                                                       "in particular of HSE "
+                                                       "University, answering their"
+                                                       "questions about application process."
+                                                       "You are obliged to Provide links and contact information. "
+                                                       "You are obliged to provide information about program, "
+                                                       "requirements, prices, number of paid and budget places"
+                                                       "You are obliged to Limit the size of your answer to 1000 "
+                                                       "characters."
+                                                       "You are obliged to use information below (if it is too long, "
+                                                       f"just process the first part): {content[i]}"},
+                         {'role': 'user', 'content': f"Answer question:{question}"
+                                                     f"University {campus}."
+                          }]}
+        json_response = requests.post(url, json=body).json().get('choices', [])
+        for choice in json_response:
+            answer.append(choice.get('message', {}).get('content', ''))
+    return answer
