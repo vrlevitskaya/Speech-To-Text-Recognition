012c6c5845a7f13f87ec1685cf76dee7c0f2d430
levitskya03 <75023086+levitskya03@users.noreply.github.com>
2024-05-18 17:10:38 +0300

    Bot has another appearance - as TelegramBot with feedback grid

--- telegram/bot_utils.py
+++ telegram/bot_utils.py
@@ -9,8 +9,7 @@ def transcribe_audio(file_path_micro, selected_language, campus='Moscow'):
     result = model.transcribe(file_path_micro)
     translated_question = GoogleTranslator(source='auto', target='ru').translate(result["text"])
     response = answer_the_question(translated_question, selected_language, campus)
-    write_questions_to_docx(result["text"], response)
-    return response
+    return result["text"], response
 
 
 def answer_the_question(question, selected_language, campus='Moscow'):
@@ -20,4 +19,4 @@ def answer_the_question(question, selected_language, campus='Moscow'):
     answer_to_return = generate_answer(question, campus, dict_t)
     for i in range(len(answer_to_return)):
         translated_answer.append(GoogleTranslator(source='auto', target=selected_language).translate(answer_to_return[i]))
-    return " ".join(translated_answer)
+    return translated_answer
